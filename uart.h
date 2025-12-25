859d0d74144b9a898bf08a5d6411d84aa6529cc3
gcygan2 <114261165+gcygan2@users.noreply.github.com>
2025-12-25 14:58:59 +0100

    Add files via upload

--- uart.h
+++ uart.h
@@ -1,3 +1,5 @@
+#ifndef UART_H
+#define UART_H
 extern void uart_put (char c);
 extern void uart_puts (const char *c);
 extern char uart_get ();
@@ -5,3 +7,4 @@ extern uint8_t uart_get_hex ();
 extern int8_t uart_get_dec ();
 extern void uart_put_hex (uint8_t h);
 extern void uart_put_dec (uint8_t h);
+#endif
